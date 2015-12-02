85061e6e5719752f64551ea37574f674e378d1ca
oxylius <noah.hendrikx@gmail.com>
2015-12-02 12:01:03 +0100

    Added Eventrepository.ts

--- Server/Webapp/source/classes/repository/eventRepository.js
+++ Server/Webapp/source/classes/repository/eventRepository.js
@@ -1,63 +1,63 @@
-define(['app/model/event'], function(Event) {
-	'use strict';
-
-	var EventRepository = function ($http) {
-	    this.urls = {
-	        all: '/api/events',
-	        get: '/api/events/:eventId',
-	        add: '/api/events',
-            edit: '/api/events/:eventId'
-	    };
-
-	    this.events = [];
-
-
-	    /**
-		 * Get all events
-		 *
-		 * @return Event[]
-		 */
-	    this.all = function (successCallback) {
-	        $http.get(this.urls.all)
-				.success(function (data) {
-				    // map applys a function on every element in the array and returns the result as new array
-				    var events = data.events.map(function (eventDTO) {
-				        return Event.createFromDTO(eventDTO);
-				    });
-				    successCallback(events);
-				});
-	    };
-	    /**
-		 * Find event by identifier
-		 *
-		 * @param string identifier
-		 */
-	    this.get = function (event, successCallback) {
-	        $http.get(this.urls.get.replace(':eventId', event))
-					.success(function (eventDTO) {
-					    successCallback(Event.createFromDTO(eventDTO));
-					});
-	    };
-
-	    /**
-		 * Add event
-		 * @param Event event
-		 */
-	    this.add = function (event, successCallback) {
-	        $http.post(this.urls.add, event)
-					.success(function (eventDTO) {
-					    successCallback(Event.createFromDTO(eventDTO));
-					});
-	    };
-
-	    this.edit = function (event, successCallback) {
-	        $http.post(this.urls.edit.replace(':eventId', event.id), event)
-					.success(function (eventDTO) {
-					    successCallback(Event.createFromDTO(eventDTO));
-					});
-	    };
-
-	};
-
-	return EventRepository;
+define(["require", "exports"], function (require, exports) {
+    var EventRepository = (function () {
+        function EventRepository() {
+        }
+        return EventRepository;
+    })();
+    define(['app/model/event'], function (Event) {
+        'use strict';
+        var EventRepository = function ($http) {
+            this.urls = {
+                all: '/api/events',
+                get: '/api/events/:eventId',
+                add: '/api/events',
+                edit: '/api/events/:eventId'
+            };
+            this.events = [];
+            /**
+             * Get all events
+             *
+             * @return Event[]
+             */
+            this.all = function (successCallback) {
+                $http.get(this.urls.all)
+                    .success(function (data) {
+                    // map applys a function on every element in the array and returns the result as new array
+                    var events = data.events.map(function (eventDTO) {
+                        return Event.createFromDTO(eventDTO);
+                    });
+                    successCallback(events);
+                });
+            };
+            /**
+             * Find event by identifier
+             *
+             * @param string identifier
+             */
+            this.get = function (event, successCallback) {
+                $http.get(this.urls.get.replace(':eventId', event))
+                    .success(function (eventDTO) {
+                    successCallback(Event.createFromDTO(eventDTO));
+                });
+            };
+            /**
+             * Add event
+             * @param Event event
+             */
+            this.add = function (event, successCallback) {
+                $http.post(this.urls.add, event)
+                    .success(function (eventDTO) {
+                    successCallback(Event.createFromDTO(eventDTO));
+                });
+            };
+            this.edit = function (event, successCallback) {
+                $http.post(this.urls.edit.replace(':eventId', event.id), event)
+                    .success(function (eventDTO) {
+                    successCallback(Event.createFromDTO(eventDTO));
+                });
+            };
+        };
+        return EventRepository;
+    });
+    return EventRepository;
 });
